ead7762a7a92d6c6de7d6302b901b958f37af865
Karthik <karthiknair@ufl.edu>
2025-09-16 02:47:05 -0400

    feat(App.js): add team project line; keep my line

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <header className="App-header">
 <<<<<<< HEAD
   	<img src={logo} className="App-logo" alt="logo" />
@@ -16,6 +17,14 @@ function App() {
         </p>	
 >>>>>>> 242861e (added new project text)
       </header>
+=======
+	<header className="App-header">
+  	<img src={logo} className="App-logo" alt="logo" />
+ 	<p>Hello World! This is our team project.</p>
+  	<p>My name is Karthik and this is my project</p>
+</header>
+
+>>>>>>> ad65361 (feat(App.js): add team project line; keep my line)
     </div>
   );
 }
